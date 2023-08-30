f94f2ef4ceac4c3d9e650fbac0e2b69f61b52aa6
MaksymBora <maxboraod@gmail.com>
2023-08-30 15:04:26 +0200

    add nav

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,20 @@
+import { NavLink, Route, Routes } from "react-router-dom";
 
 
 
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <nav>
+        <ul>
+          <li><NavLink to="/">Home</NavLink></li>
+          <li><NavLink to="/movies">Movies</NavLink></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/movies" element={<div>Movies</div>} />
+      </Routes>
     </div>
   );
 };
