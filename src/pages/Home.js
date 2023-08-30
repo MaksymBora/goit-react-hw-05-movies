e5a4743f652ec47faf53d544777851152eaa3bd6
MaksymBora <maxboraod@gmail.com>
2023-08-30 22:47:42 +0200

    add api request

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,18 @@
 import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { fetchMovies } from '../API';
 
 const Home = () => {
   useEffect(() => {
-    //   HTTP Request
+    const result = async () => {
+      try {
+        const movies = await fetchMovies();
+        console.log(movies);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    result();
   }, []);
 
   return (
