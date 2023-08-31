610a546cc437e0f0c765cf4c6d7803dd3442650e
MaksymBora <maxboraod@gmail.com>
2023-08-31 23:15:23 +0200

    add search

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,13 +1,19 @@
 import { useEffect, useState } from 'react';
 import { fetchByQuery } from '../API';
 import { MovieList } from 'components/MovieList';
+import { useSearchParams } from 'react-router-dom';
 
 const SearchMovie = () => {
+  const [query, setQuery] = useState('');
   const [queryResult, setQueryResult] = useState([]);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const inputResult = searchParams.get('searchQuery');
+
   useEffect(() => {
     const result = async () => {
       try {
-        const result = await fetchByQuery('superman');
+        const result = await fetchByQuery(query);
         setQueryResult(result);
       } catch (error) {
         console.log(error);
@@ -15,12 +21,22 @@ const SearchMovie = () => {
     };
 
     result();
-  }, []);
+  }, [query]);
+
+  const onSubmitResult = e => {
+    e.preventDefault();
+    const inputResult = searchParams.get('searchQuery');
+    setQuery(inputResult);
+  };
 
   return (
     <div>
-      <form>
-        <input type="text" />
+      <form onSubmit={onSubmitResult}>
+        <input
+          type="text"
+          value={inputResult}
+          onChange={e => setSearchParams({ searchQuery: e.target.value })}
+        />
         <button type="submit">Search</button>
       </form>
       <MovieList items={queryResult} />
